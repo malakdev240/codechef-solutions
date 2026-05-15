a6fb8c51fa307f4490a6684359a63580ec5b5b4d
Malak Ahmed <code.verse2011@gmail.com>
2026-05-15 08:50:52 +0300

    Second upload

---
+++ Chefland_Games.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int r1,r2,r3,r4;
+        cin>>r1>>r2>>r3>>r4;
+    if (r1 == 0 and r2== 0 and r3== 0 and r4== 0){
+        cout <<"in"<<endl;
+    }
+    else{
+        cout<<"out"<<endl;
+    }
+    }
+}
