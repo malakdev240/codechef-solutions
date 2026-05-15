e1bfb2416c6dac6d55d74324c89f54555ad766d5
Malak Ahmed <code.verse2011@gmail.com>
2026-05-15 08:52:56 +0300

    Add files via upload

---
+++ Expert_Setter.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if (y>=x/2.0){
+            cout <<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+    }
+}
