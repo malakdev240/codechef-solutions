aa00c3f2d540c3c959f15f5d20c2c193040f1ad9
Malak Ahmed <code.verse2011@gmail.com>
2026-05-15 08:50:22 +0300

    First upload

---
+++ Chef_in_his_office.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int total=(x*4)+y;
+        cout<< total<<endl;
+    }
+}
